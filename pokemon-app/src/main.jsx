e791000819bb642f6a73bfac0e7466265f61e59a
kevin <kevin.manno@training4u.it>
2026-07-01 15:49:25 +0200

    few

--- pokemon-app/src/main.jsx
+++ pokemon-app/src/main.jsx
@@ -11,7 +11,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
+      <BrowserRouter basename="/pokedex">
         <App />
       </BrowserRouter>
     </Provider>
